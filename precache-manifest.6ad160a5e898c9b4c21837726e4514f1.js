7af4a4980630342f691d7f9e8b2824dd070c24ec
PrayashShrestha <prayashshrestha964@gmail.com>
2020-07-19 10:29:28 +0545

    Updates

---
+++ precache-manifest.6ad160a5e898c9b4c21837726e4514f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c25ae789214fd12c5036ad19d549d5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "33371a071d8470c8c74b",
+    "url": "/monster-rolodex/static/css/main.57b41937.chunk.css"
+  },
+  {
+    "revision": "394d25692a158c53c7cf",
+    "url": "/monster-rolodex/static/js/2.9af05a07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9af05a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33371a071d8470c8c74b",
+    "url": "/monster-rolodex/static/js/main.e741a726.chunk.js"
+  },
+  {
+    "revision": "342a08363466d39828ed",
+    "url": "/monster-rolodex/static/js/runtime-main.1bce4f14.js"
+  }
+]);
